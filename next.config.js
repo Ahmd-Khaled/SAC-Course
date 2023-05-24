4051d85821524caec80e012ba195f0cf840ef28f
Ahmd-Khaled <ahmd.khaldd@gmail.com>
2023-05-25 01:28:11 +0300

    v1.1

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // env: {
+  //   MONGO_URI: "mongodb+srv://ahmdkhaldd:Et7494780@cluster0.fxmpaqv.mongodb.net/products-app",
+  //   APP_PROD: "https://ahmedkhald.com",
+  //   APP_DEV: "http://localhost:3000",
+  // },
+  images: {
+    domains: ["encrypted-tbn0.gstatic.com", "cdn.britannica.com", "images.heb.com"]
+  }
 }
 
 module.exports = nextConfig
